c2657aecba542bb277e5f23bd966ccff15938f0e
Paul P. <articasparagus@gmail.com>
2024-09-25 08:06:12 +0200

    improved build.sh for mac and linux

--- build.sh
+++ build.sh
@@ -1,26 +1,54 @@
 #!/bin/bash
 
-# Check if an argument is provided
+# Check if an argument is provided for the build directory
 if [ $# -eq 0 ]; then
     # No argument provided, use the default build directory
-    mkdir build
+    mkdir -p build
     build_dir="build"
 else
     # Argument provided, use it as the build directory
     build_dir="$1"
 fi
 
-# Construct the cmake command with the chosen build directory
-cmake_command="cmake \
-    -DLLVM_BUILD_TOOLS=ON \
-    -DLLVM_ENABLE_NEW_PASS_MANAGER=ON \
-    -DCMAKE_BUILD_TYPE=Debug \
-    -DLLVM_ENABLE_PROJECTS=\"clang;lld\" \
-    -DLLVM_ENABLE_LLD=ON \
-    -DLLVM_STATIC_LINK_CXX_STDLIB=ON \
-    -S ./llvm-project/llvm \
-    -B $build_dir \
-    -G \"Ninja\""
+# Determine the operating system
+os_type=$(uname)
 
-# Execute the cmake command
-eval $cmake_command
+if [[ "$os_type" == "Linux" ]]; then
+    # CMake command for Linux
+    linux_cmake_command="cmake \
+        -DLLVM_BUILD_TOOLS=ON \
+        -DLLVM_ENABLE_NEW_PASS_MANAGER=ON \
+        -DCMAKE_BUILD_TYPE=Debug \
+        -DLLVM_ENABLE_PROJECTS=\"clang;lld\" \
+        -DLLVM_ENABLE_LLD=ON \
+        -DLLVM_STATIC_LINK_CXX_STDLIB=ON \
+        -S ./llvm-project/llvm \
+        -B $build_dir \
+        -G \"Ninja\""
+
+    # Execute the Linux CMake command
+    eval $linux_cmake_command
+
+elif [[ "$os_type" == "Darwin" ]]; then
+    # CMake command for macOS
+    macos_cmake_command="cmake \
+        -DLLVM_BUILD_TOOLS=ON \
+        -DLLVM_ENABLE_NEW_PASS_MANAGER=ON \
+        -DCMAKE_BUILD_TYPE=Debug \
+        -DLLVM_ENABLE_PROJECTS=\"clang;lld\" \
+        -DLLVM_ENABLE_LLD=ON \
+        -DLLVM_STATIC_LINK_CXX_STDLIB=ON \
+        -DCMAKE_EXE_LINKER_FLAGS='-fuse-ld=lld' \
+        -DCMAKE_SHARED_LINKER_FLAGS='-fuse-ld=lld' \
+        -S ./llvm-project/llvm \
+        -B $build_dir \
+        -G Ninja"
+
+    # Execute the macOS CMake command
+    eval $macos_cmake_command
+
+else
+    echo "Unsupported operating system: $os_type"
+    exit 1
+fi
+
